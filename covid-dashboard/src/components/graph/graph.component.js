2c9d4beae0aa064d40f9211a804109fdcea940e7
Pavel Koryakin <unclekoin@gmail.com>
2020-12-16 22:13:03 +0300

    feat: some changes

--- covid-dashboard/src/components/graph/graph.component.js
+++ covid-dashboard/src/components/graph/graph.component.js
@@ -9,46 +9,46 @@ export class Graph {
     this.ctx = this.canvas.getContext("2d");
   }
 
-  static graph = new Chart(this.ctx, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
-
   loadGraph() {
     this.canvas.className = 'graph';
     this.graphChart.append(this.canvas);
+
+    const graph = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
   init() {
@@ -56,13 +56,15 @@ export class Graph {
   }
 
   update(currentSelect) {
-    this.countriesData = CovidDashboardService.getState().filter((item) => item.code === currentSelect)[0];
-    this.currentSelect = this.countriesData.country;
+    // this.countriesData = CovidDashboardService.getState().filter((item) => item.code === currentSelect)[0];
+    // this.currentSelect = this.countriesData.country;
     this.loadGraph();
   }
 
   viewData(data) {
-    this.countriesData = data;
+    console.log(data)
+    // this.countriesData = data;
+    // this.countriesDataStore = data;
     this.loadGraph();
   }
 }
