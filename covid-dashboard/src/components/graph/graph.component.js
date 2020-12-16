3c9b214a9ca0bf884bfd04b3834aba28f78be15a
Pavel Koryakin <unclekoin@gmail.com>
2020-12-16 18:07:38 +0300

    feat: some changes

--- covid-dashboard/src/components/graph/graph.component.js
+++ covid-dashboard/src/components/graph/graph.component.js
@@ -1,58 +1,68 @@
 import Chart from 'chart.js';
 import './graph.component.scss';
-import {CovidDashboardService} from "../../core/index";
+import { CovidDashboardService } from "../../core/index";
 
 export class Graph {
   constructor() {
     this.graphChart = document.querySelector('.graph-chart');
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  static graph = new Chart(this.ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+
+  loadGraph() {
+    this.canvas.className = 'graph';
+    this.graphChart.append(this.canvas);
   }
 
   init() {
+    CovidDashboardService.getGlobal().then((data) => this.viewData(data));
   }
 
-  update(currentSelect){
+  update(currentSelect) {
+    this.countriesData = CovidDashboardService.getState().filter((item) => item.code === currentSelect)[0];
+    this.currentSelect = this.countriesData.country;
+    this.loadGraph();
+  }
 
+  viewData(data) {
+    this.countriesData = data;
+    this.loadGraph();
   }
 }
-
-const graphChart = document.querySelector('.graph-chart');
-graphChart.innerHTML = '<canvas class="graph"></canvas>';
-
-const ctx = document.querySelector('.graph').getContext('2d');
-
-const graph = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
