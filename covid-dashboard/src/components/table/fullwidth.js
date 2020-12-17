a4bfa034f1dde78577ce2d45f78187814c49a2a3
AlexeyFL <alexeyfl563@gmail.com>
2020-12-18 00:07:37 +0300

    feat: add full screen compnent feature(test)

---
+++ covid-dashboard/src/components/table/fullwidth.js
@@ -0,0 +1,24 @@
+function createElem(elem, cls, text) {
+    const element = document.createElement(elem);
+    element.className = cls;
+    element.textContent = text;
+    return element;
+}
+
+function fullWidth() {
+    const components = document.querySelectorAll(".fullwidth");
+
+    components.forEach((component) => {
+        component.appendChild(createElem("span", "fullwidth__switcher", "✥"));
+    });
+
+    function addClass() {
+        document.addEventListener("click", (e) => {
+            if (e.target.classList.contains("fullwidth__switcher")) {
+                e.target.closest(".fullwidth").classList.toggle("active");
+            }
+        });
+    }
+    addClass();
+}
+export { fullWidth };
--- covid-dashboard/src/components/table/table.component.js
+++ covid-dashboard/src/components/table/table.component.js
@@ -1,7 +1,6 @@
 import "./table.component.scss";
 
 
-import { WORLD_POPULATION, CovidDashboardService } from "../../core/index";
 
     }
     }
