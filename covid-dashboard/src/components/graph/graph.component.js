b07a9e390290011f8164ab47dcda561b0870e873
Pavel Koryakin <unclekoin@gmail.com>
2020-12-18 21:58:52 +0300

    feat: add change data & update graph

--- covid-dashboard/src/components/graph/graph.component.js
+++ covid-dashboard/src/components/graph/graph.component.js
@@ -1,46 +1,104 @@
 import Chart from 'chart.js';
 import './graph.component.scss';
 import { CovidDashboardService } from "../../core/index";
+// import { doc } from 'prettier';
+
+Chart.defaults.global.defaultFontColor = '#bdbdbd';
 
 export class Graph {
   constructor() {
     this.graphChart = document.querySelector('.graph-chart');
+    this.arrowBlock = document.createElement('div');
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext("2d");
   }
 
-  loadGraph(data) {
+  loadGraph(externalData) {
     this.canvas.className = 'graph';
     this.graphChart.append(this.canvas);
 
-    const lables = Object.keys(data.cases);
-    const cases = Object.values(data.cases);
+    this.arrowBlock.className = 'arrow-block';
+    this.graphChart.append(this.arrowBlock);
 
-    const graph = new Chart(this.ctx, {
-      type: 'bar',
-      data: {
-        labels: lables,
-        datasets: [{
-          label: 'World Cases',
-          data: cases,
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
-        }]
+    const lables = Object.keys(externalData.cases);
+    const dataSet = Object.values(externalData.cases);
+
+    this.arrowBlock.innerHTML =
+      `<button class="arrow-block__button arrow-left"></button>
+    <div class="arrow-block__title"></div>
+    <button class="arrow-block__button arrow-right"></button>`;
+
+    const title = document.querySelector('.arrow-block__title')
+
+    const wordDate = [
+      {
+        title: 'World Daily Cases',
+        type: 'cases',
+        backgroundColor: '#f1c40f'
+      }
+      ,
+      {
+        title: 'World Daily Deaths',
+        type: 'deaths',
+        backgroundColor: '#e74c3c'
       },
-      options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+      {
+        title: 'World Daily Recovered',
+        type: 'recovered',
+        backgroundColor: '#27ae60'
+      }
+    ];
+ const updateGraph = (background = '#f1c40f', set = dataSet) => {
+    const data = {
+      labels: lables,
+      datasets: [{
+        label: 'Daily Cases',
+        data: set,
+        backgroundColor: background,
+        // borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      }]
     }
-  });
 
+    const options = {
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
 
-};
+    return new Chart(this.ctx, {
+      type: 'bar',
+      data,
+      options
+    });
+  }
+
+  updateGraph();
+
+    let counter = 1
+    title.textContent = 'World Daily Cases';
+
+    this.arrowBlock.addEventListener('click', (e) => {
+      if (e.target.classList.contains('arrow-block__button')) {
+        if (counter > wordDate.length - 1) counter = 0;
+
+        title.textContent = wordDate[counter].title;
+        const set = Object.values(externalData[wordDate[counter].type]);
+        const backgroundColor = wordDate[counter].backgroundColor;
+        updateGraph(backgroundColor, set)
+
+        counter += 1;
+      }
+    });
+
+  };
 
   init() {
     CovidDashboardService.getWorld().then((data) => this.viewData(data));
